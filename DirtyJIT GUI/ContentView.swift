0f09c2ed4ca5935d83a0d9de791da4a9d9fd3174
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-06 15:49:44 -0500

    uhm

--- DirtyJIT GUI/ContentView.swift
+++ DirtyJIT GUI/ContentView.swift
@@ -9,40 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State var showError = false
-    @discardableResult // Add to suppress warnings when you don't want/need a result
-    func safeShell(_ command: String) throws -> String {
-        let task = Process()
-        let pipe = Pipe()
-        
-        task.standardOutput = pipe
-        task.standardError = pipe
-        task.arguments = ["-c", command]
-        task.executableURL = URL(fileURLWithPath: "/bin/bash") //<--updated
-        task.standardInput = nil
-
-        try task.run() //<--updated
-        
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)!
-        
-        return output
-    }
     var body: some View {
         VStack {
             Text("Download from https://github.com/verygenericname/WDBDDISSH/actions")
             Text("Unzip into ~/Downloads/JIT")
             Text("Rename files to JIT.dmg and JIT.dmg.signature")
             Button(action: {
-                    do {
-                        print(try safeShell("ideviceimagemounter ~/Downloads/JIT/JIT.dmg ~/Downloads/JIT/JIT.dmg.signature"))
-                    }
-                    catch {
-                        showError = true
-                        print("\(error)") //handle or silence the error here
-                    }
 
                   }) {
-                    Text("Load it!").font(.body)
+                    Text("Load it!")
                   }
         }
         .alert("Error!", isPresented: $showError) {
