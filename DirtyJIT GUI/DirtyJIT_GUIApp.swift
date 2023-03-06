46a043ee92a2a841d8a3fed539a7c765b615732c
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-03-06 08:08:36 -0500

    Initial Commit

---
+++ DirtyJIT GUI/DirtyJIT_GUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirtyJIT_GUIApp.swift
+//  DirtyJIT GUI
+//
+//  Created by Hariz Shirazi on 2023-03-06.
+//
+
+import SwiftUI
+
+@main
+struct DirtyJIT_GUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
